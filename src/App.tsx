9478fa8c7d23dc6464f4804c2424cdbdf0a53f35
Shubham Upadhyay <shubhamwork48@gmail.com>
2025-07-22 11:17:03 +0530

    add google admob

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path='' />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/ads/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GoogleAdMob = () => {
+  return (
+    <div>GoogleAdMob</div>
+  )
+}
+
+export default GoogleAdMob;
